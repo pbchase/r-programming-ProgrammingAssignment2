12f65a9b388b82020a052af2b52a17a46042323b
Philip Chase <pbc@ufl.edu>
2014-09-20 13:15:39 -0400

    Write function makeCacheMatrix based on makeVector by Roger D. Peng.
    Write test for makeCacheMatrix
    Add documentation for R file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,34 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+# These functions in cachematrix.R provide a caching service layered
+# on top of R's solve() function.  This allows programs to access the
+# inverse of a large matrix with the penalty of recomputing the inverse.
+# makeCacheMatrix creates an object that stores the matrix value and
+# the some related value.
+# cacheSolve computes the inverse of a matrix, stores the inverse as
+# part of the object, and reaccess the stored value on subsequent calls.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    # makeCacheMatrix - a function to cache a matrix and the result of an
+    # operation on the matrix.  Any operation can be cached.
+    # Based on makeVector: https://github.com/rdpeng/ProgrammingAssignment2, Roger D. Peng
+    m <- NULL
+    set <- function(y) {
+            x <<- y
+            o <<- NULL
+    }
+    get <- function() x
+    setoutput <- function(output) o <<- output
+    getoutput <- function() o
+    list(set = set, get = get,
+         setoutput = setoutput,
+         getoutput = getoutput)
 }
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # cacheSolve returns a matrix that is the inverse of 'x' and cache's
+    # that value for later retrieval.
 }
---
+++ tests/testcachesolve.R
@@ -0,0 +1,37 @@
+# test functions of cachematrix.R
+
+# prepare for tests
+x <- matrix(rnorm(16), 4, 4)
+my_object <- makeCacheMatrix(x)
+
+# test methods of data storage class
+expect_identical(x,my_object$get())
+
+dummy <- 4
+my_object$setoutput(dummy)
+expect_identical(dummy,my_object$getoutput())
+
+# ----------------
+# test cache function
+
+# prepare for tests
+x <- matrix(rnorm(16), 4, 4)
+my_object <- makeCacheMatrix(x)
+
+# generate function output
+expect_identical(solve(x), cacheSolve(my_object))
+# recall cached value
+expect_identical(solve(x), cacheSolve(my_object))
+
+# check for the message that says the cache was read
+expect_that(cacheSolve(my_object), shows_message("getting cached data"))
+
+# test that a revision to the input changes the output
+x <- matrix(rnorm(25),5,5)
+my_object <- makeCacheMatrix(x)
+
+expect_identical(x,my_object$get())
+expect_identical(solve(x), cacheSolve(my_object))
+
+
+
