881cb5f338f4c55b9dab3f1f52cbd9c9968096ec
Philip Chase <pbc@ufl.edu>
2014-09-20 07:55:09 -0400

    Add tests to testmakevector.R and make it more generic

--- tests/testmakevector.R
+++ tests/testmakevector.R
@@ -1,16 +1,34 @@
-# test methods of makevector
+# test functions of makevector.R
 
+# prepare for tests
 x <- seq(1,10,1)
-my_vector <- makeVector(x)
+my_object <- makeVector(x)
 
-expect_identical(x,my_vector$get())
-expect_identical(mean(x), cachemean(my_vector))
+# test methods of data storage class
+expect_identical(x,my_object$get())
+
+dummy <- 4
+my_object$setmean(dummy)
+expect_identical(dummy,my_object$getmean())
+
+# ----------------
+# test cache function
+
+# prepare for tests
+x <- seq(1,10,1)
+my_object <- makeVector(x)
+
+# generate function output
+expect_identical(mean(x), cachemean(my_object))
+# recall cached value
+expect_identical(mean(x), cachemean(my_object))
+
+# check for the message that says the cache was read
+expect_that(cachemean(my_object), shows_message("getting cached data"))
 
 # test that a revision to the input changes the output
 x <- seq(1,100,1)
-my_vector <- makeVector(x)
+my_object <- makeVector(x)
 
-expect_identical(x,my_vector$get())
-expect_identical(mean(x), cachemean(my_vector))
-
-
+expect_identical(x,my_object$get())
+expect_identical(mean(x), cachemean(my_object))
