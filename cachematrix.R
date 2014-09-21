cfe956b06081dc5fa8b249574ac30c3b3d0b4533
Philip Chase <pbc@ufl.edu>
2014-09-21 15:26:42 -0400

    Add usage details and credits in comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,22 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-# These functions in cachematrix.R provide a caching service layered
-# on top of R's solve() function.  This allows programs to access the
-# inverse of a large matrix with the penalty of recomputing the inverse.
+# These functions in cachematrix.R provide a caching service for R's
+# solve() function.  This allows programs to access the
+# inverse of a large matrix without the penalty of recomputing the inverse.
+#
 # makeCacheMatrix creates an object that stores the matrix value and
-# the some related value.
+# some related value.
+#
 # cacheSolve computes the inverse of a matrix, stores the inverse as
-# part of the object, and reaccess the stored value on subsequent calls.
+# part of the object, and reaccesses the stored value on subsequent calls.
 
 makeCacheMatrix <- function(x = matrix()) {
     # makeCacheMatrix - a function to cache a matrix and the result of an
     # operation on the matrix.  Any operation can be cached.
+    #
+    # Usage:
+    #   Create an object and store a matrix in it
+    #     x <- rnorm(25)
+    #     a <- makeCacheMatrix(x)
+    #
+    #   Access the stored matrix
+    #     copy_of_matrix x$get()
+    #
+    #   Store the operation on the matrix
+    #     result <- some_function(x$get())
+    #     x$setoutput(result)
+    #
+    #   Access the stored result
+    #     x$getoutput()
+    #
     # Based on makeVector: https://github.com/rdpeng/ProgrammingAssignment2, Roger D. Peng
     o <- NULL
     set <- function(y) {
-            x <<- y
-            o <<- NULL
+        x <<- y
+        o <<- NULL
     }
     get <- function() x
     setoutput <- function(output) o <<- output
@@ -28,8 +43,9 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 cacheSolve <- function(x, ...) {
-    # cacheSolve returns a matrix that is the inverse of 'x' and cache's
-    # that value for later retrieval.
+    # cacheSolve returns a matrix that is the inverse of 'x' and caches
+    # that value for later retrieva using the an object of class makeCacheMatrix
+    # Based loosely on cacheMean: https://github.com/rdpeng/ProgrammingAssignment2, Roger D. Peng
     output <- x$getoutput()
     if (is.null(output)) {
         output <- solve(x$get(), ...)
